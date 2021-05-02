69535711b7c9803847eb842d55dc1b8f33785b10
NostalgiaJ <81918704+NostalgiaJ@users.noreply.github.com>
2021-05-03 04:09:58 +0900

    Add files via upload

---
+++ Asg_02_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>					
+
+int main()
+{
+	int vOne = 13;
+	int vTwo = 3;
+	printf("%d + %d = %d\n", vOne, vTwo, vOne + vTwo);
+	printf("%d - %d = %d\n", vOne, vTwo, vOne - vTwo);
+	printf("%d * %d = %d\n", vOne, vTwo, vOne * vTwo);
+	printf("%d / %d = %d\n", vOne, vTwo, vOne / vTwo);
+	printf("%d %% %d = %d\n", vOne, vTwo, vOne % vTwo);
+	return 0;
+}
